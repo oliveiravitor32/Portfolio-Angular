8e7652321931ed6ff28554eae4b652057fba2451
7VitorOliveira <oliveiravitor327@gmail.com>
2023-09-21 12:58:34 -0300

    Set responsivity for the Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 	titleIntroduction: string = 'Breve introdução sobre mim';
 	descriptionIntroduction: string =
-		' awodnbawuidbn aiwdbiuawbdiuaw daiuwdhaoiuwdhoiawndiuawh dawiudhawioudhaiowndiouan';
+		' Primeiramente obrigado por adentrar a meu portfólio, agora gostaria de me apresentar, sou estudante de programação desde 2022 focado em desenvolvimento front-end, atualmente estou cursando Ciências da Computação na Universidade Paulista - Unip e busco uma oportunidade no mercado para evoluir na carreira como desenvolvedor.';
 
 	titleSocialMedia: string = 'Entre em contato';
-	descriptionSocialMedia: string = 'Você pode saber mais sobre mim em';
+	descriptionSocialMedia: string = 'Você pode saber mais sobre mim em: ';
 
 	primaryColor: string = '';
 	secundaryColor: string = '';
