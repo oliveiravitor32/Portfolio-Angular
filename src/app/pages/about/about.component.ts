333b9f22aca65a24fa934570f954b3dc041ee021
Vitor Oliveira <80297830+oliveiravitor32@users.noreply.github.com>
2023-09-21 22:21:20 -0300

    Fix image path in about.component.ts to project cards

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
 	styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-	titleAbout: string = 'tituloooooo';
-	descriptionAbout: string = 'descriçãososoaodoa';
+	titleAbout: string = 'título';
+	descriptionAbout: string = 'descrição';
 
 	cardStars_1: number = 4;
 	cardTitle_1: string = 'HTML5';
@@ -15,25 +15,25 @@ export class AboutComponent {
 
 	cardStars_2: number = 3;
 	cardTitle_2: string = 'CSS3';
-	cardImage_2: string = '../../../assets/css3';
+	cardImage_2: string = '../../../assets/css3.svg';
 
 	cardStars_3: number = 3;
 	cardTitle_3: string = 'JavaScript';
-	cardImage_3: string = '../../../assets/javascript';
+	cardImage_3: string = '../../../assets/javascript.svg';
 
 	cardStars_4: number = 2;
 	cardTitle_4: string = 'TypeScript';
-	cardImage_4: string = '../../../assets/typescript';
+	cardImage_4: string = '../../../assets/typescript.svg';
 
 	cardStars_5: number = 2;
 	cardTitle_5: string = 'NodeJS';
-	cardImage_5: string = '../../../assets/nodejs';
+	cardImage_5: string = '../../../assets/nodejs.svg';
 
 	cardStars_6: number = 2;
 	cardTitle_6: string = 'Angular';
-	cardImage_6: string = '../../../assets/angular';
+	cardImage_6: string = '../../../assets/angular.svg';
 
 	cardStars_7: number = 1;
 	cardTitle_7: string = 'React';
-	cardImage_7: string = '../../../assets/react';
+	cardImage_7: string = '../../../assets/react.svg';
 }
