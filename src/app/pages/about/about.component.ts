f5cb1782e009313444ec08a53bc6490f9d3b1372
7VitorOliveira <oliveiravitor327@gmail.com>
2023-09-28 14:01:56 -0300

    Create text page about, style and add responsivity to card-tec-component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
 	styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-	titleAbout: string = 'título';
-	descriptionAbout: string = 'descrição';
+	titleAbout: string = 'Sobre mim';
+	descriptionAbout: string =
+		'Me chamo Vitor Oliveira sou estudante de programação desde 2022 focado em desenvolvimento front-end com experiências em HTML5, CSS3, JavaScript, Git, Angular e React desenvolvidas em cursos online, projetos em meu GitHub e também com o ensino superior onde atualmente estou cursando Ciências da Computação na Universidade Paulista (Unip).';
 
 	cardStars_1: number = 4;
 	cardTitle_1: string = 'HTML5';
@@ -36,4 +37,16 @@ export class AboutComponent {
 	cardStars_7: number = 1;
 	cardTitle_7: string = 'React';
 	cardImage_7: string = '../../../assets/react.svg';
+
+	toolStars_1: number = 0;
+	toolTitle_1: string = 'Windows';
+	toolImage_1: string = '../../../assets/windows.svg';
+
+	toolStars_2: number = 0;
+	toolTitle_2: string = 'Github';
+	toolImage_2: string = '../../../assets/github.svg';
+
+	toolStars_3: number = 0;
+	toolTitle_3: string = 'Visual Studio Code';
+	toolImage_3: string = '../../../assets/vscode.png';
 }
