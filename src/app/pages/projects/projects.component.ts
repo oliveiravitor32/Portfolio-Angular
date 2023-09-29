efb04d42a153ceb3a58a3437b040b684e146ef53
7VitorOliveira <oliveiravitor327@gmail.com>
2023-09-28 22:20:43 -0300

    Create responsivity to page about section-introduction and setup page projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+	selector: 'app-projects',
+	templateUrl: './projects.component.html',
+	styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
-
+	titleMain: string = 'Projetos';
+	descriptionMain: string =
+		'Está área contém alguns de meus projetos destaques.';
 }
