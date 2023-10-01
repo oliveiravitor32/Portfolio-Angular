68e90b226ed24a68d92d532475f89f169720aba2
7VitorOliveira <oliveiravitor327@gmail.com>
2023-10-01 16:48:22 -0300

    Created page projects

--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-project',
-  templateUrl: './card-project.component.html',
-  styleUrls: ['./card-project.component.css']
+	selector: 'app-card-project',
+	templateUrl: './card-project.component.html',
+	styleUrls: ['./card-project.component.css'],
 })
 export class CardProjectComponent {
-
+	@Input() image: string = '';
+	@Input() title: string = '';
+	@Input() description: string = '';
+	@Input() codeLink: string = '';
+	@Input() demoLink: string = '';
 }
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,4 +9,35 @@ export class ProjectsComponent {
 	titleMain: string = 'Projetos';
 	descriptionMain: string =
 		'Está área contém alguns de meus projetos destaques.';
+
+	imageProject_1: string = '../../../assets/NoteAppAngular.png';
+	titleProject_1: string = 'Angular - Note App';
+	descriptionProject_1: string =
+		'Projeto Bloco de Notas (NoteApp) construído utilizando Angular com navegação de página única (single page application) e possibilita buscas por notas, criar, deletar e editar notas.';
+	codeLink_1: string = 'https://github.com/oliveiravitor32/Note-App';
+	demoLink_1: string = 'https://note-app-oliveiravitor32.vercel.app/';
+
+	imageProject_2: string = '../../../assets/ShoppingCartReactJs.png';
+	titleProject_2: string = 'React - Shopping Cart';
+	descriptionProject_2: string =
+		'Projeto Carrinho de Compras (Shopping Cart) construído com ReactJS utilizando a API do Mercado Livre, desenvolvido com o canal Manual do Dev no Youtube.';
+	codeLink_2: string =
+		'https://github.com/oliveiravitor32/shopping-cart-reactjs';
+	demoLink_2: string =
+		'https://shopping-cart-reactjs-oliveiravitor32.vercel.app/';
+
+	imageProject_3: string = '../../../assets/ToDoListReactJs.png';
+	titleProject_3: string = 'React - To Do List';
+	descriptionProject_3: string =
+		'Simples projeto de uma lista de tarefas(To Do List) utilizando ReactJS, que permite criar, concluir, editar, e também deletar tarefas.';
+	codeLink_3: string = 'https://github.com/oliveiravitor32/ToDoList-ReactJS';
+	demoLink_3: string =
+		'https://to-do-list-react-js-oliveiravitor32.vercel.app/';
+
+	imageProject_4: string = '../../../assets/antigoPortfolio.png';
+	titleProject_4: string = 'Antigo portfólio';
+	descriptionProject_4: string =
+		'Antigo portfólio com página responsiva, implementações de animações via CSS e JS e com formulário funcional. Desenvolvido com o curso "Html-Css-Feliz" de Marco Bruno Dev no Youtube. ';
+	codeLink_4: string = 'https://github.com/oliveiravitor32/Antigo-Portfolio';
+	demoLink_4: string = 'https://oliveiravitor32.github.io/Antigo-Portfolio/';
 }
