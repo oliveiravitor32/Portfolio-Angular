fb3ddade68104006c87a1d15e0ab530980cfd635
Vitor Oliveira <80297830+oliveiravitor32@users.noreply.github.com>
2023-11-29 13:57:44 -0300

    Update introduction about me text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 	titleIntroduction: string = 'Breve introdução sobre mim';
 	descriptionIntroduction: string =
-		' Primeiramente obrigado por adentrar a meu portfólio, agora gostaria de me apresentar, sou estudante de programação desde 2022 focado em desenvolvimento front-end, atualmente estou cursando Ciências da Computação na Universidade Paulista - Unip e busco uma oportunidade no mercado para evoluir na carreira como desenvolvedor.';
+		'Inicialmente, expresso meus agradecimentos por acessar o meu portfólio. Permita-me, então, apresentar-me. Tenho 19 anos e sou estudante de programação desde 2022, com foco no desenvolvimento front-end. Atualmente, estou matriculado no curso de Ciências da Computação na Universidade Paulista - UNIP. Busco ativamente uma oportunidade no mercado de trabalho para aprimorar minha trajetória profissional como desenvolvedor.';
 
 	titleSocialMedia: string = 'Entre em contato';
 	descriptionSocialMedia: string = 'Você pode saber mais sobre mim em: ';
