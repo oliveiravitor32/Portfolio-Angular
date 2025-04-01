5ce76d0b0a45873e163bc3717433fbc26f753e11
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-04-01 18:48:40 -0300

    feat(ui): cria componente profile-card e sua estrutura e estilo inicial
    - Cria o componente profile-card para resumo rápido de informações do portfólio
    - Adiciona imagem de perfil
    - Adiciona ícone de download
    - Adiciona arquivo de CV

---
+++ src/app/components/profile-card/profile-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  templateUrl: './profile-card.component.html',
+  styleUrls: ['./profile-card.component.css']
+})
+export class ProfileCardComponent {
+
+}
